49e7e4060431e2a438c1160f1c6925d51f7ccef5
Anita Calmday <anita.calmday@bison.howard.edu>
2017-11-30 14:26:08 -0500

    Update subprogram1.asm
    Edit subprogram1

--- subprogram1.asm
+++ subprogram1.asm
@@ -1,8 +1,14 @@
-subprogram_1: 
-
-bgt $t1, '9', invalid
-	sub $t3, $t1, '0' #get numeric digit value
-	mul $t3, $t3, $t4 #mulitply by the current power of 16
-	add $t2, $t2, $t3
-	mul $t4, $t4, 16 #next power of 16
-	j loop
+subprogram_1:
+	sll $t2, $t2, 4					#Multiply by 16
+	
+	slti $t4, $t3, ':'				#Check if character is a number
+	bne $t4, $zero, num_subprogram_1		#Take care of character being a number case
+	
+	slti $t4, $t3, 'G'				#Check if the character is uppercase
+	bne $t4, $zero, upper_subprogram_1		#Take care of character being uppercase
+	
+	addi $t4, $t3, -87				#Subtract 87 from lowercase to get hexadecimal value
+	add $t2, $t2, $t4				#Add translated character to running sum
+	
+	jr $ra							#Return to subprogram_2
+	
