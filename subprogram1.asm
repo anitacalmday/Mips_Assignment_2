0999f7a617791ac52ac0c68993cdb9f40c4fdf9b
Anita Calmday <anita.calmday@bison.howard.edu>
2017-12-01 13:50:46 -0500

    Update subprogram1.asm
    Updated subprogram 1

--- subprogram1.asm
+++ subprogram1.asm
@@ -12,3 +12,19 @@ subprogram_1:
 	
 	jr $ra							#Return to subprogram_2
 	
+num_subprogram_1:
+	addi $t4, $t3, -48				#Subract 48 from number to get hexadecimal value					
+	add $t2, $t2, $t4				#Add translated character to running sum
+	jr $ra							#Return to subprogram_2
+	
+
+upper_subprogram_1:
+	addi $t4, $t3, -55				#Subract 55 from uppercase to get hexadecimal value
+	add $t2, $t2, $t4				#Add translated character to running sum
+	sub $t4, $t3, 'A'
+	add $t4, $t4, 10
+	mul $t3, $t3, $t4 #mulitply by the current power of 16
+	add $t2, $t2, $t3 #add to decimal
+	mul $t4, $t4, 16 #next power of 16
+	jr $ra							#Return to subprogram_2
+	
